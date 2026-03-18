0a0011443a1f2a70b544cad496089737d314fe5d
Mark Volkov <markasch67@gmail.com>
2026-03-18 15:34:04 +0300

    redirect fix x2

--- lab6/admin/index.php
+++ lab6/admin/index.php
@@ -1,11 +1,4 @@
 <?php
-// HTTP Basic Authentication
-if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-    header('WWW-Authenticate: Basic realm="Admin Panel"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo 'Требуется авторизация';
-    exit;
-}
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/functions.php';
